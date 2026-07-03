81bd0ed9407ac780a68488e970a9a298aef22682
AL2277 <n913724685@gmail.com>
2026-07-03 02:24:57 +0200

    display

--- package_route/startup.lua
+++ package_route/startup.lua
@@ -57,6 +57,35 @@ file.close()
 local this_station = this_address.station
 local this_house = this_address.house
 
+local name_list = {}
+
+for name, _ in pairs(address_book) do
+    name_list[#name_list] = name
+end
+
+local TABSIZE <const> = 16
+local COLUMNS <const> = 5
+
+local selected = 0
+
+local function display_names()
+    monitor.setBackgroundColor(colors.black)
+    monitor.setTextColor(colors.white)
+    for i=0,(#name_list-1) do
+        if i == selected then
+            monitor.setBackgroundColor(colors.white)
+            monitor.setTextColor(colors.black)
+        end
+        monitor.setCursorPos(TABSIZE * (i % COLUMNS) + 1, i // COLUMNS + 1)
+        monitor.write(name_list[i])
+        if i == selected then
+            monitor.setBackgroundColor(colors.black)
+            monitor.setTextColor(colors.white)
+        end
+        monitor.write(TABSIZE - #(name_list[i]))
+    end
+end
+
 local function send_package_to_address(station, house)
     -- TODO: set status to sending
 
@@ -64,11 +93,11 @@ local function send_package_to_address(station, house)
         input_inv.pushItems(peripheral.getName(output_inv), slot)
     end
 
-    packager.setAddress("post-" .. this_station .. station .. house)
+    packager.setAddress("post-" .. this_station .. "-" .. station .. "-" .. house)
 
     while next(output_inv.list()) ~= nil do
         packager.makePackage()
-        os.pullEvent("package_sent")
+        sleep(0.1)
     end
 
     -- TODO: change status to normal
@@ -81,5 +110,7 @@ local function send_package_to_name(name)
     end
     send_package_to_address(address_book[name].station, address_book[name].house)
 end
+
+display_names()
 
 send_package_to_name("Albert")
