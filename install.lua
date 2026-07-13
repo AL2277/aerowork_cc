47d6cfa706bc9f69548ef23c1a4b406befe8b5ec
AL2277 <n913724685@gmail.com>
2026-07-13 21:03:09 +0200

    added version for installer

--- install.lua
+++ install.lua
@@ -1,19 +1,40 @@
+VERSION_MAJOR = 1
+VERSION_MINOR = 0
+VERSION_PATCH = 1
+
+VERSION = string.format("%s.%s.%s", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
+
 local argv = {...}
 local argc = #argv
 
+local request, err, code = http.get("https://raw.githubusercontent.com/AL2277/aerowork_cc/master/program_list.json")
+
+if request == nil then
+    print("failed to access program list")
+    print("error: " .. err)
+    print("code: " .. code)
+    return
+end
+
+local data = textutils.unserializeJSON(request.readAll())
+
+if data.installer_version ~= VERSION then
+    while true do
+        write("Installer outdated, update installer?")
+        local ans = read()
+        if ans == "y" then
+            shell.run("wget https://raw.githubusercontent.com/AL2277/aerowork_cc/master/install.lua install")
+            shell.run("install", ...)
+            return
+        elseif ans == "n" then
+            break
+        end
+        write("invalid choice")
+    end
+end
+
 if argc >= 1 then
     local program = argv[1]
-
-    local request, err, code = http.get("https://raw.githubusercontent.com/AL2277/aerowork_cc/master/program_list.json")
-
-    if request == nil then
-        print("failed to access program list")
-        print("error: " .. err)
-        print("code: " .. code)
-        return
-    end
-
-    local data = textutils.unserializeJSON(request.readAll())
 
     if data[program] == nil then
         print("Unknown program: " .. program)
