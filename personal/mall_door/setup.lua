b65257ba888d83c76d4e8131659dae6fff2b3dab
AL2277 <n913724685@gmail.com>
2026-07-18 16:53:15 +0200

    mall_door

--- personal/mall_door/setup.lua
+++ personal/mall_door/setup.lua
@@ -1,1 +1,21 @@
+local terminal = require("utils.terminal")
+
 print("setting up")
+
+local require_name = true
+
+if fs.exists("/peripheral_names.json") then
+    print("Peripheral names list found.")
+    require_name = terminal.yes_no("Overwrite? (y/n): ")
+end
+
+if require_name then
+    local file = fs.open("/peripheral_names.json", "w")
+    file.write(textutils.serialize({
+        gearshift = "redstone_relay_x",
+        clutch = "redstone_relay_x",
+        bearing = "Create_MechanicalBearing_x",
+        player = "player_detector_x",
+    }))
+    file.close()
+end
--- personal/mall_door/startup.lua
+++ personal/mall_door/startup.lua
@@ -1,20 +1,67 @@
 package.path = package.path .. ";/packages"
+local std = require("std")
+std.include("priority_queue")
 
-local rot = peripheral.find("Create_SequencedGearshift")
-local gearshift = peripheral.find("redstone_relay_x")
-local gearshift = peripheral.find("redstone_relay_x")
 
-assert(rot ~= nil)
+local file = fs.open("/peripheral_names.json", "r")
+local peripheral_names = textutils.unserialize(file.readAll())
+file.close()
 
-function r(x)local s;if x>0 then g.setOutput("bottom", false) else g.setOutput("bottom", true) end; c.setOutput("bottom", true);sleep(math.abs(x)*0.05);c.setOutput("bottom", false)end
 
--- assume inital state is closed
+local gearshift = peripheral.wrap(peripheral_names.gearshift)
+local clutch = peripheral.wrap(peripheral_names.clutch)
+local bearing = peripheral.wrap(peripheral_names.bearing)
+local player = peripheral.wrap(peripheral_names.player)
 
-local stack = {}
+-- 0: open
+-- 1: close
+-- the correct state to be in
+local state = 0
 
-local function open()
-    if (#stack) % 2 == 1 then
-        return
+TOL = 1
+
+while true do
+    if #(player.getPlayersInRange(10)) > 0 then
+        state = 0
+    else
+        state = 1
     end
-
+    clutch.setOutput("bottom", false)
+    local angle = bearing.getAngle()
+    if angle < 0-TOL or (state == 1 and angle < 51 - TOL) then
+        gearshift.setOutput("bottom", true)
+        clutch.setOutput("bottom", true)
+    elseif angle > 51 + TOL or (state == 0 and angle > 0 + TOL) then
+        gearshift.setOutput("bottom", false)
+        clutch.setOutput("bottom", true)
+    end
+    os.sleep(0.05)
 end
+
+-- g=peripheral.wrap("redstone_relay_2");c=peripheral.wrap("redstone_relay_3");b=peripheral.wrap("Create_MechanicalBearing_0")
+-- function r(x)sleep(0.05);if x>0 then g.setOutput("bottom", false) else g.setOutput("bottom", true) end; c.setOutput("bottom", true);sleep(math.abs(x)*0.05);c.setOutput("bottom", false)end
+
+
+-- function open() g.setOutput("bottom", false);c.setOutput("bottom", true);while math.abs(b.getAngle()-0)>0.1 do os.sleep(0.05) end c.setOutput("bottom",false) end
+-- function close() g.setOutput("bottom", true);c.setOutput("bottom", true);while math.abs(b.getAngle()-51)>0.1 do os.sleep(0.05) end c.setOutput("bottom",false) end
+-- g.setOutput("bottom", true);c.setOutput("bottom", true);sleep(0.1);c.setOutput("bottom", false)
+
+-- -- assume inital state is closed
+
+-- local function time()
+--     return os.epoch() / 3600
+-- end
+
+-- local function comp(a, b)
+--     return a.time < b.time
+-- end
+
+-- local events = std.priority_queue{comp = comp}
+
+-- while true do
+--     local cur_time = time()
+--     while events:top().time == time do
+--         -- process events
+--     end
+--     sleep(0)
+-- end
